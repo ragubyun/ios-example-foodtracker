2b2648017c75c550f78d8c4d415d2b13ca3a3a22
ragubyun <ragubyun@gmail.com>
2016-11-30 13:54:18 +0900

    add Custom Control: RatingControl add Data Model: Meal add first test case

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,30 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by ACT on 2016. 11. 30..
+//  Copyright © 2016년 RaguByun. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,23 +9,71 @@
 import UIKit
 
 class RatingControl: UIView {
+    
+    // MARK: Properties
+    
+    var rating = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    var ratingButtons = [UIButton]()
+    
+    let spacing = 5
+    let starCount = 5
 
     // MARK: Initialization
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.red
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
-        addSubview(button)
+        let filledStarImage = UIImage(named: "filledStar")
+        let emptyStarImage = UIImage(named: "emptyStar")
         
+        for _ in 0..<starCount {
+            let button = UIButton()
+            button.setImage(emptyStarImage, for: .normal)
+            button.setImage(filledStarImage, for: .selected)
+            button.setImage(filledStarImage, for: [.highlighted, .selected])
+//            button.backgroundColor = UIColor.red
+            button.adjustsImageWhenHighlighted = false
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: UIControlEvents.touchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override func layoutSubviews() {
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        for (index, button) in ratingButtons.enumerated() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
+            button.frame = buttonFrame
+        }
+        
+        updateButtonSelectionStates()
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
     }
     
     // MARK: Button Action
     
     func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        rating = ratingButtons.index(of: button)! + 1
+        
+        updateButtonSelectionStates()
+    }
+    
+    func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+        }
     }
 
 }
