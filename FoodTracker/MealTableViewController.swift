e82e5dcbf54a14c0bd0c236e9ee8f1745d08af15
Rain <ragubyun@gmail.com>
2016-12-09 16:27:41 +0900

    implement table view cell deletion

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -16,6 +16,8 @@ class MealTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = editButtonItem
 
         loadSampleMeals()
     }
@@ -60,6 +62,21 @@ class MealTableViewController: UITableViewController {
         cell.ratingControl.rating = meal.rating
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            meals.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
     }
 
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
