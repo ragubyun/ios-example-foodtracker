3e316adb792f68b39cf958aa5458aacf5bfe83da
RaguByun <ragubyun@gmail.com>
2016-12-12 02:21:07 +0900

    persist data save, load 전까지 작업

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     // MARK: Properties
     
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    // MARK: Archiving Paths
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    // TODO: 이부분 swift3 로 바꾸기
+//    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+    
+    // MARK: Types
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
     
     // MARK: Initialization
     
@@ -23,9 +37,27 @@ class Meal {
         self.photo = photo
         self.rating = rating
         
+        super.init()
+        
         if name.isEmpty || rating < 0 {
             return nil
         }
+    }
+    
+    // MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.nameKey)
+        aCoder.encode(photo, forKey: PropertyKey.photoKey)
+        aCoder.encode(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObject(forKey: PropertyKey.nameKey) as! String
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.ratingKey)
+        
+        self.init(name: name, photo: photo, rating: rating)
     }
     
 }
